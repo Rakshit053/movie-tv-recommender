36efd20084b3ca780b80402895c94171a667a2b0
Rakshit053 <rakshitraj0053@gmail.com>
2026-08-31 17:33:29 -0400

    rank movies using imdb weighted rating formula

--- explore_data.py
+++ explore_data.py
@@ -29,4 +29,15 @@ print("\nTop 10 that people have actually seen:")
 popular = movies[mask]
 best_popular = popular.sort_values('vote_average', ascending=False)
 print(best_popular[['title', 'vote_average', 'vote_count']].head(10)) 
- 
+C = movies['vote_average'].mean()
+m = movies['vote_count'].quantile(0.90)
+
+qualified = movies[movies['vote_count'] >= m].copy()
+
+v = qualified['vote_count']
+R = qualified['vote_average']
+qualified['score'] = (v / (v + m)) * R + (m / (v + m)) * C
+
+top = qualified.sort_values('score', ascending=False)
+print("\nTop 10 by weighted rating:")
+print(top[['title', 'vote_average', 'vote_count', 'score']].head(10))
